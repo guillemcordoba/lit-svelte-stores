e97f7fcc1a4b6c301c2dd639a731a4b996b0d8c4
Guillem Cordoba <guillem.cordoba@gmail.com>
2021-08-30 14:47:32 +0200

    Added context-provided

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,3 @@
-import { Readable, Unsubscriber } from "svelte/store";
-import { LitElement, ReactiveController } from "lit";
-
-export class StoreController<T> implements ReactiveController {
-  value!: T;
-
-  private _unsubscribe!: Unsubscriber;
-
-  constructor(protected host: LitElement, protected store: Readable<T>) {
-    host.addController(this);
-  }
-
-  hostConnected() {
-    this._unsubscribe = this.store.subscribe((value) => {
-      this.value = value;
-      this.host.requestUpdate();
-    });
-  }
-
-  hostDisconnected() {
-    this._unsubscribe();
-  }
-}
+export * from "./store-controller";
+export * from "./store-controller";
+export * from "./context-provided-store";
